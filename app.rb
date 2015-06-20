10e3e08b18786574bddc0e76f52df08e4fc7d408
chenlizhan <chung1350@hotmail.com>
2015-06-21 01:05:51 +0800

    Production mode for base url

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class SightAdvisorApp < Sinatra::Base
   enable :sessions
   register Sinatra::Flash
 
-  BASE_URL = 'http://localhost:4567'
+  # BASE_URL = 'http://localhost:4567'
+  BASE_URL = 'https://sightadvisor.herokuapp.com'
   get '/' do
     @result = HTTParty.get("#{BASE_URL}/api/v1/keywords");
     erb :index
