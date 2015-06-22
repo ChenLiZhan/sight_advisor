94c563547b735e340aa0ea77c43e63fd0a547593
chenlizhan <chung1350@hotmail.com>
2015-06-22 16:00:37 +0800

    Uniqe and random keywords

--- app.rb
+++ app.rb
@@ -7,16 +7,21 @@ class SightAdvisorApp < Sinatra::Base
   enable :sessions
   register Sinatra::Flash
 
-  # BASE_URL = 'http://localhost:4567'
-  BASE_URL = 'http://sightadvisor.herokuapp.com'
+  BASE_URL = 'http://localhost:4567'
+  # BASE_URL = 'http://sightadvisor.herokuapp.com'
   get '/' do
-    @result = HTTParty.get("#{BASE_URL}/api/v1/keywords");
+    @result = HTTParty.get("#{BASE_URL}/api/v1/keywords")
     erb :index
   end
   
   get '/api/v1/keywords' do
     content_type :json, 'charset' => 'utf-8'
     s = File.read(File.join('public', 'keyword2.txt'))
-    s
+    keyword_ary = []
+    s = JSON.parse(s)
+    s.each do |value|
+      keyword_ary << value['keywords']
+    end
+    keyword_ary.flatten.uniq.shuffle.to_json
   end
 end
